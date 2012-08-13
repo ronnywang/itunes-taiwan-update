86388d7d896dc9f0dc5fe1f84ae602397226d22b
Ronny Wang <ronnywang@gmail.com>
2012-08-14 01:01:21 +0800

    add model path

--- webdata/init.inc.php
+++ webdata/init.inc.php
@@ -1,6 +1,9 @@
 <?php
 
 include(__DIR__ . '/libs/pixframework/Pix/Loader.php');
-set_include_path(__DIR__ . '/libs/pixframework');
+set_include_path(
+    __DIR__ . '/libs/pixframework'
+    . PATH_SEPARATOR . __DIR__ . '/models'
+);
 
 Pix_Loader::registerAutoload();
