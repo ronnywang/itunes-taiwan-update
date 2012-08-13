deb856e5c6b1110feaeae7baea9010b9cc45e77b
Ronny Wang <ronnywang@gmail.com>
2012-08-14 01:01:28 +0800

    add db config

--- webdata/init.inc.php
+++ webdata/init.inc.php
@@ -7,3 +7,17 @@ set_include_path(
 );
 
 Pix_Loader::registerAutoload();
+
+if (file_exists(__DIR__ . '/config.php')) {
+    include(__DIR__ . '/config.php');
+} else {
+    if (getenv('DATABASE_URL')) {
+        if (preg_match('#postgres://([^:]*):([^@]*)@([^/]*)/(.*)#', strval(getenv('DATABASE_URL')), $matches)) {
+            $user = $matches[1];
+            $pass = $matches[2];
+            $host = $matches[3];
+            $dbname = $matches[4];
+            Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_PgSQL(array('user' => $user, 'password' => $pass, 'host' => $host, 'dbname' => $dbname)));
+        }
+    }
+}
