847a3adf1ea95d3a5f8984052e67e447d9d3925e
Ronny Wang <ronnywang@gmail.com>
2012-08-14 01:01:53 +0800

    add Movie model

---
+++ webdata/models/Movie.php
@@ -0,0 +1,16 @@
+<?php
+
+class Movie extends Pix_Table
+{
+    public function init()
+    {
+        $this->_primary = 'id';
+        $this->_name = 'movie';
+
+        // http://itunes.apple.com/tw/movie/bian-fu-xia-kai-zhan-shi-ke/id534463913?l=zh
+        // => 534463913
+        $this->_columns['id'] = array('type' => 'int');
+        $this->_columns['url'] = array('type' => 'text');
+        $this->_columns['description'] = array('type' => 'text');
+    }
+}
